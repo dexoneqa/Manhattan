e2e5c3ce01fbd4ea90c30173152cff7a07fafea6
quantumtom <cornyn@gmail.com>
2013-01-17 13:16:56 -0800

    Refactoring files.

--- js/site.js
+++
@@ -1,8 +0,0 @@
-/*global $, hs */
-$(".trNavEmail").click(function () {
-    hs.htmlExpand(this, {contentId: "Email-html"});
-});
-
-$(".trNavSend").click(function () {
-    hs.htmlExpand(this, {contentId: "Phone-html"});
-});
